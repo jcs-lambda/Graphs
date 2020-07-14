26527d6bb37f8cae48fea901a326299dfd46fc5c
jcs-lambda <57103874+jcs-lambda@users.noreply.github.com>
2020-07-14 14:36:55 -0500

    refactors ancestor.py

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -1,53 +1,62 @@
 from queue import LifoQueue
 
-def earliest_ancestor(ancestors, starting_node):
-    # create adjacency list mapping each child to their parents
-    parents = {}
-    for parent, child in ancestors:
-        values = parents.setdefault(child, [])
-        values.append(parent)
+# http://code.activestate.com/recipes/579103-python-addset-attributes-to-list/
+class ParentChildList(list):
+    def __new__(self, *args, **kwargs):
+        return super(ParentChildList, self).__new__(self, args, kwargs)
 
-    # default: no parent found
-    earliest = -1
+    def __init__(self, *args, **kwargs):
+        if len(args) == 1 and hasattr(args[0], '__iter__'):
+            list.__init__(self, args[0])
+        else:
+            list.__init__(self, args)
+        self.__dict__.update(kwargs)
+        self.parents = set()
+        self.children = set()
+        for parent, child in self:
+            self.parents.add(parent)
+            self.children.add(child)
 
-    # ensure initial child has a parent
-    if starting_node in parents:
-        # track parentage
-        lineages = []
-        # initialze queue
+    def __call__(self, **kwargs):
+        self.__dict__.update(kwargs)
+        return self
+
+    def has_parent(self, person):
+        return person in self.children
+
+    def get_parents(self, person):
+        parents = []
+        for parent, child in self:
+            if person == child:
+                parents.append(parent)
+        return parents
+    
+    def lineage(self, person):
+        if not self.has_parent(person):
+            return -1
+        lineages = {}
         queue = LifoQueue()
-        queue.put([starting_node])
-        # begin depth first search
+        queue.put([person])
         while not queue.empty():
-            # get current parentage line
             lineage = queue.get()
-            # looking for parents of last child in line
-            child = lineage[-1]
-            # if no known parents
-            if child not in parents:
-                # store complete lineage
-                lineages.append(lineage)
-                # look no further
+            person = lineage[-1]
+            if not self.has_parent(person):
+                temp = lineages.setdefault(len(lineage), [])
+                temp.append(lineage)
                 continue
-            # has parents, so for each known parent
-            for parent in parents[child]:
-                # append parent to current lineage
-                new_lineage = lineage + [parent]
-                # queue updated lineage to search for further parents
-                queue.put(new_lineage)
-
-        # find furthest ancestor (last entry in longest lineage)
-        # store as list to get the lowest numbered from oldest generation
-        oldest = []
-        max_generations = 0
-        for lineage in lineages:
-            generations = len(lineage)
-            if generations > max_generations:
-                max_generations = generations
-                oldest = [lineage[-1]]
-            elif generations == max_generations:
-                oldest.append(lineage[-1])
+            for parent in self.get_parents(person):
+                queue.put(lineage + [parent])
         
-        earliest = min(oldest)
+        return lineages
 
-    return earliest
+    def earliest_ancestors(self, person):
+        lineages = self.lineage(person)
+        return -1 if lineages == -1 else [
+            lineage[-1] for lineage in lineages[max(lineages)]
+        ]
+
+
+def earliest_ancestor(ancestors, starting_node):
+    pcl = ParentChildList(*ancestors)
+    earliest = pcl.earliest_ancestors(starting_node)
+    return -1 if earliest == -1 else min(earliest)
